cda38ae9129f59973e559826727ae6b8ed8a0463
xuzhuoyi <xzy476386434@vip.qq.com>
2017-01-27 00:06:14 +0800

    Moved const position in PendSV

--- portable/gcc/ARM_CM4F/port.c
+++ portable/gcc/ARM_CM4F/port.c
@@ -11,6 +11,7 @@
 extern l_uint32_t PSP_array[3];
 extern l_uint8_t l_nextTaskID;
 extern l_uint8_t Cur_TaskID;
+l_uint8_t l_tmpNextID = 0;
 
 
 void LPortSysTickHandler( void )
@@ -22,31 +23,35 @@ void LPortSysTickHandler( void )
 
 void LPortPendSVHandler( void )
 {
+	l_tmpNextID = l_nextTaskID;
 	__asm volatile
 	(
+			"	.align 4							\n"
+			    "Cur_TaskIDConst:   .word Cur_TaskID    \n"
+				"PSP_arrayConst:    .word PSP_array     \n"
+				"l_nextTaskIDConst: .word l_tmpNextID  \n"
 	"	mrs r0, psp							\n"
 	"	isb									\n"
 	"										\n"
-	"	stmdb  r0!, {r4-r11,r14}			    \n"
+	"	stmdb  r0!, {r4-r11}			    \n"
 	"	ldr    R1,Cur_TaskIDConst           \n"
     "   ldr    R3,PSP_arrayConst            \n"
     "   ldr    R4,l_nextTaskIDConst         \n"
-	"										\n"
+	"	ldrb	r2,[r1]					    \n"
+	"   str    R0,[R3, R2, LSL #2]          \n"
 	"	LDRB    R4,[R4]                      \n"
-	//"   STR    R4,[R1]                      \n"
+	"   STRB    R4,[R1]                      \n"
 	"   LDR    R0,[R3, R4, LSL #2]          \n"
-	"   LDMIA  R0!,{R4-R11,r14}                 \n"
+
+	"   LDMIA  R0!,{R4-R11}                 \n"
     "                                       \n"
 	"	msr psp, r0							\n"
 	"	isb									\n"
 	"										\n"
 	"										\n"
-	"	bx r14								\n"
+	"	bx lr								\n"
 	"										\n"
-	"	.align 4							\n"
-    "Cur_TaskIDConst:   .word Cur_TaskID    \n"
-	"PSP_arrayConst:    .word PSP_array     \n"
-	"l_nextTaskIDConst: .word l_nextTaskID  \n"
+
 	:
 	:
 	:
--- src/task.c
+++ src/task.c
@@ -16,8 +16,8 @@ l_uint8_t l_nextTaskID = 0;
 
 l_uint32_t PSP_array[3];
 
-l_uint32_t task0_stack[18];
-l_uint32_t task1_stack[18];
+l_uint32_t task0_stack[128];
+l_uint32_t task1_stack[128];
 
 
 extern void Task_0();	 //任务0
@@ -36,25 +36,26 @@ l_uint32_t LTaskIncrementTick( void )
 	if(++osTick == LCONFIG_OS_TIMESLICE)
 	{
 		 osTick = 0;
-		 if(++l_nextTaskID >= 3)
+		 if(++l_nextTaskID >= 2)
 			 l_nextTaskID = 0;
 	}
 }
 
 l_uint32_t LTaskCreate( void )
 {
-	PSP_array[0] = ((unsigned int) task0_stack) + (sizeof task0_stack) - 17*4;
+	l_uint32_t tmp = sizeof task0_stack;
+	PSP_array[0] = ((unsigned int) task0_stack) + (sizeof task0_stack) - 16*4;
 	    //PSP_array中存储的为task0_stack数组的尾地址-16*4，即task0_stack[1023-16]地址
-	    HW32_REG((PSP_array[0] + (15<<2))) = (unsigned long) Task_0; /* PC */
+	    HW32_REG((PSP_array[0] + (14<<2))) = (unsigned long) Task_0; /* PC */
 	    //task0的PC存储在task0_stack[1023-16]地址  +14<<2中，即task0_stack[1022]中
-	    HW32_REG((PSP_array[0] + (16<<2))) = 0x01000000;            /* xPSR */
-	    HW32_REG((PSP_array[0] + (8<<2))) = (unsigned long) Task_0;            /* LR */
+	    HW32_REG((PSP_array[0] + (15<<2))) = 0x01000000;            /* xPSR */
+	   // HW32_REG((PSP_array[0] + (13<<2))) = (unsigned long) Task_0;            /* LR */
 
 
-	    PSP_array[1] = ((unsigned int) task1_stack) + (sizeof task1_stack) - 17*4;
-	    HW32_REG((PSP_array[1] + (15<<2))) = (unsigned long) Task_1; /* PC */
-	    HW32_REG((PSP_array[1] + (16<<2))) = 0x01000000;            /* xPSR */
-	    HW32_REG((PSP_array[1] + (8<<2))) = (unsigned long) Task_1;            /* LR */
+	    PSP_array[1] = ((unsigned int) task1_stack) + (sizeof task1_stack) - 16*4;
+	    HW32_REG((PSP_array[1] + (14<<2))) = (unsigned long) Task_1; /* PC */
+	    HW32_REG((PSP_array[1] + (15<<2))) = 0x01000000;            /* xPSR */
+	   // HW32_REG((PSP_array[1] + (13<<2))) = (unsigned long) Task_1;            /* LR */
 
 	Cur_TaskID = 0;
 
@@ -65,7 +66,7 @@ l_uint32_t LTaskCreate( void )
 void LTaskStartScheduler( void )
 {
 	/* 设置PSP指向任务0堆栈的栈顶 */
-	__asm volatile ("MSR psp, %0\n" : : "r" ((PSP_array[Cur_TaskID] + 16*4)) : "sp");
+	__asm volatile ("MSR psp, %0\n" : : "r" ((PSP_array[Cur_TaskID] + 16*4)) :"sp" );
 
 	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
@@ -75,10 +76,12 @@ void LTaskStartScheduler( void )
 
 	  /* SysTick_IRQn interrupt configuration */
 	  HAL_NVIC_SetPriority(SysTick_IRQn, 15, 0);
+	  HAL_NVIC_SetPriority(PendSV_IRQn, 15, 0);
 	__asm volatile ("MSR control, %0" : : "r" (0x3) : "memory");
 
 	__asm volatile ("isb 0xF":::"memory");
 
-		    Task_0();
+		    //Task_0();
+	__asm volatile ("b Task_0");
 
 }
