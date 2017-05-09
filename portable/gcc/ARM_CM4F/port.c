5480e2b842fc175180e5bcc1b9fdcbe365e74a61
xuzhuoyi <xzy476386434@vip.qq.com>
2017-05-09 18:40:29 +0800

    Added LPortInitStack() in ARM_CM4F/port.c

--- portable/gcc/ARM_CM4F/port.c
+++ portable/gcc/ARM_CM4F/port.c
@@ -30,3 +30,36 @@ void LPortInitScheduler(void)
 	__asm volatile ("b Task_0");
 
 }
+
+l_stack_t *LPortInitStack(l_stack_t *pxTopOfStack, LTaskFunction_t pxEntry)
+{
+    l_stack_t  *p_stk;
+    l_uint32_t  *p_stk32;
+                                                                    /* Load and pre-align stack pointer.                    */
+    p_stk = pxTopOfStack;
+    p_stk++;
+    p_stk32 = (l_uint32_t *)p_stk;
+                                                                    /* Save registers as if auto-saved.                     */
+                                                                    /* Follow stacking method in "9) Perform automatic ...  */
+                                                                    /* ..context save" of section 3.4 in "SPRU430E"         */
+    *p_stk32++ = 0x01000000;                                    /*   T:ST0                                              */
+    *p_stk32++ = (l_uint32_t)pxEntry;                           /*   AH:AL                                              */
+    *p_stk32++ = 0x55554444;                                    /*   PH:PL                                              */
+    *p_stk32++ = 0x77776666;                                    /*   AR1:AR0                                            */
+    *p_stk32++ = 0x99998888;                                    /*   DP:ST1                                             */
+    *p_stk32++ = 0xBBBBAAAA;                                    /*   DBGSTAT:IER                                        */
+    *p_stk32++ = 0xDDDDCCCC;                                    /*   Save Return Address [PC+1].                        */
+                                                                    /* Save remaining registers.                            */
+    *p_stk32++ = 0x77776666;                                    /*   AR1H:AR0H                                          */
+    *p_stk32++ = 0x99999999;                                    /*   XAR2                                               */
+    *p_stk32++ = 0xAAAAAAAA;                                    /*   XAR3                                               */
+    *p_stk32++ = 0xBBBBBBBB;                                    /*   XAR4: void * parameter.                            */
+    *p_stk32++ = 0xCCCCCCCC;                                    /*   XAR5                                               */
+    *p_stk32++ = 0xDDDDDDDD;                                    /*   XAR6                                               */
+    *p_stk32++ = 0xEEEEEEEE;                                    /*   XAR7                                               */
+    *p_stk32++ = 0xFFFFFFFF;                                    /*   XT                                                 */
+    *p_stk32++ = 0x00000000;                                    /*   RPC                                                */
+                                                                /* Return pointer to next free location.                */
+    return ((l_stack_t *)p_stk32);
+
+}
--- portable/gcc/ARM_CM4F/portmacro.h
+++ portable/gcc/ARM_CM4F/portmacro.h
@@ -9,6 +9,8 @@
 #define PORTABLE_PORTMACRO_H_
 
 #include <stdint.h>
+#include "stm32f4xx.h"
+#include "core_cm4.h"
 
 typedef uint8_t l_uint8_t;
 typedef uint16_t l_uint16_t;
@@ -21,6 +23,9 @@ typedef int32_t l_int32_t;
 typedef int64_t l_int64_t;
 
 typedef uint32_t l_stack_t;
+typedef l_uint32_t l_handle_t;
+typedef l_uint32_t l_sp_t;
+typedef l_uint32_t l_base_t;
 
 #define LPORT_NVIC_INT_CTRL_REG		( * ( ( volatile uint32_t * ) 0xe000ed04 ) )
 #define LPORT_NVIC_PENDSVSET_BIT	( 1UL << 28UL )
